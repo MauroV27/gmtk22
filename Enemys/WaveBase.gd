7d3d638ba31897405648fbb4ebc64bb18809fcdb
Necco-Z <necco@outlook.com>
2022-07-16 19:30:11 -0300

    Reorganização de arquivos

--- Enemys/WaveBase.gd
+++ Enemys/WaveBase.gd
@@ -1,9 +1,11 @@
 extends Node
 
+signal wave_ended
+
 export (float) var stage_interval = 10
 
 var current_stage := 0
-var wave_ended := false
+var wave_ending := false
 
 onready var stages = $Stages.get_children() as Array
 onready var points = $SpawnPoints.get_children() as Array
@@ -13,14 +15,23 @@ func _ready() -> void:
 	for i in stages:
 		for j in i.get_children():
 			j.global_position = points[randi() % points.size()].global_position
-	start_stage()
 
 
 func _process(_delta: float) -> void:
-	if wave_ended:
+	if wave_ending:
 		var stage = stages[current_stage] as Node
 		if stage.get_child_count() == 0:
 			queue_free()
+
+
+func start_wave():
+	start_stage()
+
+
+func set_target(target: KinematicBody2D):
+	for i in stages:
+		for j in i.get_children():
+			j._target = target
 
 
 func start_stage():
@@ -29,11 +40,12 @@ func start_stage():
 		i.activate()
 	var time = get_tree().create_timer(stage_interval)
 	time.connect("timeout", self, "on_timeout")
+	print(name, ": Stage ", current_stage + 1, " started")
 
 
 func _on_timeout():
 	if current_stage == stages.size() - 1:
-		wave_ended = true
+		wave_ending = true
 	else:
 		current_stage += 1
 		start_stage()
