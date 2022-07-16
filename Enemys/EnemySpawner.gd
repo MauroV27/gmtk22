34cb5a8985d15aa7d9e0084c7da6ea23503fcfa7
Mauro Victor <maurovictor13@gmail.com>
2022-07-16 19:42:02 -0300

    Adjust enemy spawner for debug with new enemys.

--- Enemys/EnemySpawner.gd
+++ Enemys/EnemySpawner.gd
@@ -1,6 +1,8 @@
 extends Node
 
 const ENEMY = preload("res://Enemys/Types/EnemyType1.tscn")
+const ENEMY_2 = preload("res://Enemys/Types/EnemyType2.tscn")
+const ENEMY_3 = preload("res://Enemys/Types/EnemyType3.tscn")
 
 onready var timer : Timer = $Timer
 var _player_target_ref : KinematicBody2D
@@ -13,7 +15,15 @@ func start_spawn() -> void:
 	timer.start()
 
 func create_enemy() -> void:
-	var _enemy = ENEMY.instance()
+	var _enemy 
+	
+	match randi() % 3:
+		0:
+			_enemy = ENEMY.instance()
+		1:
+			_enemy = ENEMY_2.instance()
+		2:
+			_enemy = ENEMY_3.instance()
 	
 	var _type : int = randi() % Global.object_types.size()
 	var _pos : Vector2 = Vector2( rand_range(-400, 400), rand_range(-200, 200) )
